a59581b14c0faec30bba9346b8986cbc4cc82c04
Roberto Moreda <moreda@allenta.com>
2014-06-23 18:39:17 +0200

    Fixed doc

--- scripts/pull_subtrees.sh
+++ scripts/pull_subtrees.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Add subtrees provided in STDIN, one per line and two columns:
+# Pull subtrees provided in STDIN, one per line and two columns:
 # prefix remote_name branch
 awk '
 !/^($|[:space:]*#)/ {
