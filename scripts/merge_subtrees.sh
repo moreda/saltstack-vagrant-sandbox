c8bf0e386aab7530da38cd12cda170f08b7b8ecc
Roberto Moreda <moreda@allenta.com>
2014-06-23 12:15:26 +0200

    Fixes in scripts

--- scripts/merge_subtrees.sh
+++ scripts/merge_subtrees.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Merge subtrees provided in STDIN, one per line and two columns:
-# prefix remote_name branch
+# remote_name prefix branch
 awk '
 !/^($|[:space:]*#)/ {
-  cmd = "git subtree merge -P " $2 " " $1 " " $3 " --squash";
+  cmd = "git subtree merge -P " $2 " " $1 "/" $3 " --squash";
   print cmd;
   system(cmd);
 }
